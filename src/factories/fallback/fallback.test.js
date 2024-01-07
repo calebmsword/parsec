356716d758560b8f9ad60b6e70ab111a16ac5b51
Caleb Sword <caleb.m.sword@gmail.com>
2024-01-07 18:32:11 -0500

    fallback implemented + tested

---
+++ src/factories/fallback/fallback.test.js
@@ -0,0 +1,67 @@
+import assert from "node:assert";
+import { describe, test, mock } from "node:test";
+
+import { fallback } from "./fallback.js";
+
+mock.timers.enable(["setTimeout"]);
+
+describe("fallback", () => {
+    test("throws if no requestors are provided", () => {
+        assert.throws(() => fallback([]));
+        // @ts-ignore, intentional wrong type 
+        assert.throws(() => fallback());
+    });
+
+    test("requestors are executed in order", () => {
+        /** @type {number[]} */
+        const requestorResults = [];
+
+        fallback([
+            receiver => {
+                requestorResults.push(1);
+                receiver({ reason: "fail" });
+            },
+            receiver => {
+                requestorResults.push(2);
+                receiver({ value: "success" });
+            }
+        ])(_result => {
+            assert.deepStrictEqual([1, 2], requestorResults);
+        });
+        mock.timers.tick(0);
+    });
+    
+    test("the fallback succeeds with the value of first success", () => {
+        fallback([
+            receiver => receiver({ reason: "fail" }),
+            receiver => receiver({ value: "value" })
+        ])(({ value }) => {
+            assert.strictEqual("value", value);
+        });
+        mock.timers.tick(0);
+    });
+
+    test("the fallback fails only if every requestor fails", () => {
+        fallback([
+            receiver => receiver({ reason: "fail" }),
+            receiver => receiver({ reason: "fail" })
+        ])(({ value }) => {
+            assert.strictEqual(undefined, value);
+        });
+        mock.timers.tick(0);
+    });
+
+    test("the fallback fails if no successes before timeLimit", () => {
+        fallback([
+            receiver => {
+                setTimeout(() => receiver({ value: "value" }), 100);
+            }
+        ], {
+            timeLimit: 10
+        })(({ value }) => {
+            assert.strictEqual(undefined, value);
+        });
+        mock.timers.tick(0);
+        mock.timers.tick(100);
+    });
+});
